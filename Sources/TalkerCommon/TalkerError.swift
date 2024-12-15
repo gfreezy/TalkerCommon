36a3c6aefdc312380a62c8f92ea2345a972a2d17
Alex.F <gfreezy@gmail.com>
2024-12-15 18:06:50 +0800

    add file,line,column to error

--- Sources/TalkerCommon/TalkerError.swift
+++ Sources/TalkerCommon/TalkerError.swift
@@ -21,10 +21,17 @@ public struct TalkerError: LocalizedError {
     /// A localized message providing "help" text if the user requests help.
     public var helpAnchor: String?
     
-    public init(_ errorDescription: String? = nil, failureReason: String? = nil, recoverySuggestion: String? = nil, helpAnchor: String? = nil) {
-        self.errorDescription = errorDescription
+    let file: String
+    let line: Int
+    let column: Int
+
+    public init(_ errorDescription: String? = nil, failureReason: String? = nil, recoverySuggestion: String? = nil, helpAnchor: String? = nil, file: String = #file, line: Int = #line, column: Int = #column) {
+        self.errorDescription = "\(file):\(line):\(column)\n\(errorDescription ?? "")"
         self.failureReason = failureReason
         self.recoverySuggestion = recoverySuggestion
         self.helpAnchor = helpAnchor
+        self.file = file
+        self.line = line
+        self.column = column
     }
 }
