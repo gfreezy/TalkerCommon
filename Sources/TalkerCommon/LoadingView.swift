a3b986a9d02a413c0dd3609f19dc6340086196fa
Alex.F <gfreezy@gmail.com>
2025-01-10 23:04:13 +0800

    update

--- Sources/TalkerCommon/LoadingView.swift
+++ Sources/TalkerCommon/LoadingView.swift
@@ -9,7 +9,8 @@
 import SwiftUI
 
 public struct LoadingView<T: Sendable, Content: View>: View {
-    @Loadable<T> var data
+    @Loadable<T> var loadableData
+    let data: T?
     let content: (T) -> Content
     let task: @MainActor (Loadable<T>.Binding) async -> Void
     @State var inited: Bool = false
@@ -20,23 +21,22 @@ public struct LoadingView<T: Sendable, Content: View>: View {
     ) {
         self.content = content
         self.task = task
-    }
-
-    public init(
-        @ViewBuilder content: @escaping (T) -> Content,
-        task: @MainActor @escaping (Loadable<T>.Binding) async -> Void
-    ) {
-        self.content = content
-        self.task = task
+        self.data = nil
     }
 
     public init(
         _ data: T,  // 不能去掉这个参数，用来触发 parent 的 rerender，这样会重新构造 content，保证数据为最新的数据
         @ViewBuilder content: @escaping (T) -> Content,
-        task: @MainActor @escaping (Loadable<T>.Binding) async -> Void
+        task: @MainActor @escaping () async throws -> Void
     ) {
         self.content = content
-        self.task = task
+        self.task = { @Sendable @MainActor (loader: Loadable<T>.Binding) async -> Void in
+            await loader.load {
+                try await task()
+                return data
+            }
+        }
+        self.data = data
     }
 
     public init(
@@ -49,11 +49,12 @@ public struct LoadingView<T: Sendable, Content: View>: View {
                 try await task()
             }
         }
+        self.data = nil
     }
 
     public var body: some View {
         Group {
-            switch data {
+            switch loadableData {
             case .absent, .loading:
                 ProgressView()
                     .controlSize(.large)
@@ -65,18 +66,22 @@ public struct LoadingView<T: Sendable, Content: View>: View {
                     Text("Error: \(error)")
                     Button("Retry") {
                         Task {
-                            await task($data)
+                            await task($loadableData)
                         }
                     }
                     Spacer()
                 }
             case .loaded(let d):
-                content(d)
+                if let data = data {
+                    content(data)
+                } else {
+                    content(d)
+                }
             }
         }
         .task {
             if !inited {
-                await task($data)
+                await task($loadableData)
                 inited = true
             }
         }
